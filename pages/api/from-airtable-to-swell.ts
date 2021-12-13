53fd9dfab93106ee47f2a0aa336a30dffb4598e4
BisiDavi <oludavidconnect@gmail.com>
2021-12-13 22:47:36 +0100

    api updated

--- pages/api/from-airtable-to-swell.ts
+++ pages/api/from-airtable-to-swell.ts
@@ -15,67 +15,69 @@ export default async function createSwellProductHandler(
   res: NextApiResponse
 ) {
   let count = 0;
-  switch (req.method) {
-    case "GET": {
-      base("To Shopify")
-        .select({
-          maxRecords: 7794,
-          view: "Grid view",
-          filterByFormula: "NOT({Product Type} = '')",
-        })
-        .eachPage(
-          function page(records, fetchNextPage) {
-            try {
-              records.forEach(async function (record: any) {
-                if (record.fields) {
-                  const formattedUrl = record.fields["Image Src"]?.split(";");
-                  if (formattedUrl) {
-                    formattedUrlArray(formattedUrl, record).then(
-                      async (response) => {
-                        const productData = toShopifyProductModel(
-                          record.fields,
-                          response
-                        );
-                        await swell
-                          .post("/products", productData)
-                          .then((response: any) => {
-                            count = count + 1;
-                            console.log(
-                              "count",
-                              count,
-                              "response createSwellProductHandler",
-                              response
-                            );
-                            return res.status(200).json(response);
-                          })
-                          .catch((error: any) => {
-                            console.error(
-                              "error createSwellProductHandler",
-                              error
-                            );
-                          });
-                      }
-                    );
+  return new Promise<void>((resolve) => {
+    switch (req.method) {
+      case "GET": {
+         base("To Shopify")
+          .select({
+            maxRecords: 7794,
+            view: "Grid view",
+            filterByFormula: "NOT({Product Type} = '')",
+          })
+          .eachPage(
+            function page(records, fetchNextPage) {
+              try {
+                records.forEach(async function (record: any) {
+                  if (record.fields) {
+                    const formattedUrl = record.fields["Image Src"]?.split(";");
+                    if (formattedUrl) {
+                      await formattedUrlArray(formattedUrl, record).then(
+                        async (response) => {
+                          const productData = toShopifyProductModel(
+                            record.fields,
+                            response
+                          );
+                          await swell
+                            .post("/products", productData)
+                            .then(() => {
+                              count = count + 1;
+                              console.log(
+                                "count",
+                                count,
+                                "response createSwellProductHandler"
+                              );
+                            })
+                            .catch((error: any) => {
+                              console.error(
+                                "error createSwellProductHandler",
+                                error
+                              );
+                            });
+                        }
+                      );
+                    }
                   }
-                }
-              });
-            } catch (e) {
-              console.log("error inside each page", e);
-              return res.status(400).json(e);
+                });
+              } catch (e) {
+                console.log("error inside each page", e);
+                return res.status(400).send(e);
+              }
+              fetchNextPage();
+            },
+            function done(err) {
+              if (err) {
+                res.status(400).json({ status: err });
+                console.error(err);
+                resolve();
+                return res.status(400).send(err);
+              } else {
+                console.log("All products uploaded successfully");
+                resolve();
+                return res.status(200).send({ status: "ok" });
+              }
             }
-            fetchNextPage();
-          },
-          function done(err) {
-            if (err) {
-              res.status(400).json({ status: err });
-              console.error(err);
-              return res.status(400).json(err);
-            } else {
-              console.log("All products uploaded successfully");
-              return res.status(200).json({ status: "ok" });
-            }
-          }
-        );
+          );
+      }
     }
-  }
+  });
 }
