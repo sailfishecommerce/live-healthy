ac64baf9d2309dbbb8fb0c417f5f5936457c8208
BisiDavi <oludavidconnect@gmail.com>
2021-12-16 12:20:55 +0100

    homepage slider updated

--- lib/swellNode.ts
+++ lib/swellNode.ts
@@ -1,8 +1,5 @@
 import swell from "swell-node";
 
 export default function swellNodeInit() {
-  swell.init(
-    "sailfish-e-commerce-limited",
-    `${process.env.NEXT_PUBLIC_SWELL_SECRET_KEY}`
-  );
+  swell.init("sailfish-e-commerce-limited", "5qBYeK0FS6djOP7TzCWOQ5hWQZZzzvnr");
 }
