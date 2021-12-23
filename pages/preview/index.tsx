f81b19a2edb4776180d247d96f88f73df8227122
BisiDavi <oludavidconnect@gmail.com>
2021-12-23 19:45:39 +0100

    update component

--- pages/preview/index.tsx
+++ pages/preview/index.tsx
@@ -21,8 +21,7 @@ interface PropsType {
   products: productType[];
 }
 
-// export default function Index({ products }: PropsType) {
-export default function Index() {
+export default function Index({ products }: PropsType) {
   const { createVboutCart } = useVbout();
   const dispatch = useAppDispatch();
   const vboutSlice = useAppSelector((state) => state.integrations);
@@ -65,7 +64,7 @@ export default function Index() {
         sliderImg3="/img/home/hero-slider/03.jpg"
       />
       <PopularCategories />
-      {/* <TrendingProductsPreview products={products} /> */}
+      <TrendingProductsPreview products={products} />
       <FeaturedCategory
         categoryTitle="Shop for medicine"
         categoryCaption="Get started now"
@@ -104,16 +103,16 @@ export default function Index() {
   );
 }
 
-// export async function getServerSideProps() {
-//   swellNodeInit();
-//   const products = await swell.get("/products", {
-//     where: { select_store: "livehealthy" },
-//     limit: 30,
-//   });
+export async function getServerSideProps() {
+  swellNodeInit();
+  const products = await swell.get("/products", {
+    where: { select_store: "livehealthy" },
+    limit: 30,
+  });
 
-//   return {
-//     props: {
-//       products: products.results,
-//     },
-//   };
-// }
+  return {
+    props: {
+      products: products.results,
+    },
+  };
+}
