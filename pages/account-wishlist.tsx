4dc699bf21696910cda93fce55c0c8d0b5eba148
BisiDavi <oludavidconnect@gmail.com>
2022-01-12 23:51:46 +0100

    account-wishlist optimized

--- pages/account-wishlist.tsx
+++ pages/account-wishlist.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import dynamic from "next/dynamic";
 
 import Applayout from "@/layout/Applayout";
+import WishlistItem from "@/components/WishlistItem";
+import wishlistItemContent from "@/json/wishlist-items.json";
+
+const DynamicDashboardSidebar = dynamic(
+  () => import("../components/DashboardSidebar")
+);
 
 export default function AccountWishlist() {
   return (
@@ -37,118 +44,7 @@ export default function AccountWishlist() {
       <div className="container pb-5 mb-2 mb-md-4">
         <div className="row">
           {/*<!-- Sidebar-->*/}
-          <aside className="col-lg-4 pt-4 pt-lg-0 pe-xl-5">
-            <div className="bg-white rounded-3 shadow-lg pt-1 mb-5 mb-lg-0">
-              <div className="d-md-flex justify-content-between align-items-center text-center text-md-start p-4">
-                <div className="d-md-flex align-items-center">
-                  <div
-                    className="img-thumbnail rounded-circle position-relative flex-shrink-0 mx-auto mb-2 mx-md-0 mb-md-0"
-                    style={{ width: "6.375rem" }}
-                  >
-                    <span
-                      className="badge bg-warning position-absolute end-0 mt-n2"
-                      data-bs-toggle="tooltip"
-                      title="Reward points"
-                    >
-                      384
-                    </span>
-                    <img
-                      className="rounded-circle"
-                      src="/img/shop/account/avatar.jpg"
-                      alt="Susan Gardner"
-                    />
-                  </div>
-                  <div className="ps-md-3">
-                    <h3 className="fs-base mb-0">Susan Gardner</h3>
-                    <span className="text-accent fs-sm">
-                      s.gardner@example.com
-                    </span>
-                  </div>
-                </div>
-                <a
-                  className="btn btn-primary d-lg-none mb-2 mt-3 mt-md-0"
-                  href="#account-menu"
-                  data-bs-toggle="collapse"
-                  aria-expanded="false"
-                >
-                  <i className="ci-menu me-2"></i>Account menu
-                </a>
-              </div>
-              <div className="d-lg-block collapse" id="account-menu">
-                <div className="bg-secondary px-4 py-3">
-                  <h3 className="fs-sm mb-0 text-muted">Dashboard</h3>
-                </div>
-                <ul className="list-unstyled mb-0">
-                  <li className="border-bottom mb-0">
-                    <Link href="/account-orders" passHref>
-                      <a className="nav-link-style d-flex align-items-center px-4 py-3">
-                        <i className="ci-bag opacity-60 me-2"></i>
-                        Orders
-                        <span className="fs-sm text-muted ms-auto">1</span>
-                      </a>
-                    </Link>
-                  </li>
-                  <li className="border-bottom mb-0">
-                    <a
-                      className="nav-link-style d-flex align-items-center px-4 py-3 active"
-                      href="account-wishlist"
-                    >
-                      <i className="ci-heart opacity-60 me-2"></i>
-                      Wishlist
-                      <span className="fs-sm text-muted ms-auto">3</span>
-                    </a>
-                  </li>
-                  <li className="mb-0">
-                    <Link href="/account-tickets" passHref>
-                      <a className="nav-link-style d-flex align-items-center px-4 py-3">
-                        <i className="ci-help opacity-60 me-2"></i>
-                        Support tickets
-                        <span className="fs-sm text-muted ms-auto">1</span>
-                      </a>
-                    </Link>
-                  </li>
-                </ul>
-                <div className="bg-secondary px-4 py-3">
-                  <h3 className="fs-sm mb-0 text-muted">Account settings</h3>
-                </div>
-                <ul className="list-unstyled mb-0">
-                  <li className="border-bottom mb-0">
-                    <Link href="/account-profile" passHref>
-                      <a className="nav-link-style d-flex align-items-center px-4 py-3">
-                        <i className="ci-user opacity-60 me-2"></i>
-                        Profile info
-                      </a>
-                    </Link>
-                  </li>
-                  <li className="border-bottom mb-0">
-                    <a
-                      className="nav-link-style d-flex align-items-center px-4 py-3"
-                      href="account-address"
-                    >
-                      <i className="ci-location opacity-60 me-2"></i>
-                      Addresses
-                    </a>
-                  </li>
-                  <li className="mb-0">
-                    <Link href="/account-payment" passHref>
-                      <a className="nav-link-style d-flex align-items-center px-4 py-3">
-                        <i className="ci-card opacity-60 me-2"></i>
-                        Payment methods
-                      </a>
-                    </Link>
-                  </li>
-                  <li className="d-lg-none border-top mb-0">
-                    <Link href="/account-signin" passHref>
-                      <a className="nav-link-style d-flex align-items-center px-4 py-3">
-                        <i className="ci-sign-out opacity-60 me-2"></i>
-                        Sign out
-                      </a>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </aside>
+          <DynamicDashboardSidebar />
           {/*<!-- Content  -->*/}
           <section className="col-lg-8">
             {/*<!-- Toolbar-->*/}
@@ -162,115 +58,9 @@ export default function AccountWishlist() {
                 </a>
               </Link>
             </div>
-            {/*<!-- Wishlist-->*/}
-            {/*<!-- Item-->*/}
-            <div className="d-sm-flex justify-content-between mt-lg-4 mb-4 pb-3 pb-sm-2 border-bottom">
-              <div className="d-block d-sm-flex align-items-start text-center text-sm-start">
-                <Link href="/shop-single-v1" passHref>
-                  <a
-                    className="d-block flex-shrink-0 mx-auto me-sm-4"
-                    style={{ width: "10rem" }}
-                  >
-                    <img src="/img/shop/cart/02.jpg" alt="Product" />
-                  </a>
-                </Link>
-                <div className="pt-2">
-                  <h3 className="product-title fs-base mb-2">
-                    <Link href="/shop-single-v1" passHref>
-                      <a>TH Jeans City Backpack</a>
-                    </Link>
-                  </h3>
-                  <div className="fs-sm">
-                    <span className="text-muted me-2">Brand:</span>
-                    Tommy Hilfiger
-                  </div>
-                  <div className="fs-sm">
-                    <span className="text-muted me-2">Color:</span>
-                    Khaki
-                  </div>
-                  <div className="fs-lg text-accent pt-2">
-                    $79.<small>50</small>
-                  </div>
-                </div>
-              </div>
-              <div className="pt-2 ps-sm-3 mx-auto mx-sm-0 text-center">
-                <button className="btn btn-outline-danger btn-sm" type="button">
-                  <i className="ci-trash me-2"></i>Remove
-                </button>
-              </div>
-            </div>
-            {/*<!-- Item-->*/}
-            <div className="d-sm-flex justify-content-between my-4 pb-3 pb-sm-2 border-bottom">
-              <div className="d-block d-sm-flex align-items-start text-center text-sm-start">
-                <Link href="/shop-single-v1" passHref>
-                  <a
-                    className="d-block flex-shrink-0 mx-auto me-sm-4"
-                    style={{ width: "10rem" }}
-                  >
-                    <img src="/img/shop/cart/03.jpg" alt="Product" />
-                  </a>
-                </Link>
-                <div className="pt-2">
-                  <h3 className="product-title fs-base mb-2">
-                    <Link href="/shop-single-v1" passHref>
-                      <a>3-Color Sun Stash Hat</a>
-                    </Link>
-                  </h3>
-                  <div className="fs-sm">
-                    <span className="text-muted me-2">Brand:</span>
-                    The North Face
-                  </div>
-                  <div className="fs-sm">
-                    <span className="text-muted me-2">Color:</span>
-                    Pink / Beige / Dark blue
-                  </div>
-                  <div className="fs-lg text-accent pt-2">
-                    $22.<small>50</small>
-                  </div>
-                </div>
-              </div>
-              <div className="pt-2 ps-sm-3 mx-auto mx-sm-0 text-center">
-                <button className="btn btn-outline-danger btn-sm" type="button">
-                  <i className="ci-trash me-2"></i>Remove
-                </button>
-              </div>
-            </div>
-            {/*<!-- Item-->*/}
-            <div className="d-sm-flex justify-content-between mt-4">
-              <div className="d-block d-sm-flex align-items-start text-center text-sm-start">
-                <Link href="/shop-single-v1" passHref>
-                  <a
-                    className="d-block flex-shrink-0 mx-auto me-sm-4"
-                    style={{ width: "10rem" }}
-                  >
-                    <img src="/img/shop/cart/04.jpg" alt="Product" />
-                  </a>
-                </Link>
-                <div className="pt-2">
-                  <h3 className="product-title fs-base mb-2">
-                    <Link href="/shop-single-v1" passHref>
-                      <a>Cotton Polo Regular Fit</a>
-                    </Link>
-                  </h3>
-                  <div className="fs-sm">
-                    <span className="text-muted me-2">Size:</span>
-                    42
-                  </div>
-                  <div className="fs-sm">
-                    <span className="text-muted me-2">Color:</span>
-                    Light blue
-                  </div>
-                  <div className="fs-lg text-accent pt-2">
-                    $9.<small>00</small>
-                  </div>
-                </div>
-              </div>
-              <div className="pt-2 ps-sm-3 mx-auto mx-sm-0 text-center">
-                <button className="btn btn-outline-danger btn-sm" type="button">
-                  <i className="ci-trash me-2"></i>Remove
-                </button>
-              </div>
-            </div>
+            {wishlistItemContent.map((content) => (
+              <WishlistItem key={content.image} content={content} />
+            ))}
           </section>
         </div>
       </div>
