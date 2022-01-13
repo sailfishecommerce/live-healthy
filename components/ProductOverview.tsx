7425762801c0df76b906d6884251b2f0df06fed9
BisiDavi <oludavidconnect@gmail.com>
2022-01-13 12:59:26 +0100

    optimizing product review

--- components/ProductOverview.tsx
+++ components/ProductOverview.tsx
@@ -1,6 +1,7 @@
 import ProductBanner from "./ProductBanner";
 import ProductDescription from "./ProductDescription";
 import ProductGalleryDetails from "./ProductGalleryDetails";
+import ProductReviews from "./ProductReviews";
 
 interface ProductOverviewProps {
   pageProduct?: any;
@@ -12,7 +13,7 @@ export default function ProductOverview({ pageProduct }: ProductOverviewProps) {
       <ProductBanner product={pageProduct} />
       <ProductGalleryDetails product={pageProduct} />
       <ProductDescription product={pageProduct} />
-      {/* <ProductReviews product={pageProduct} /> */}
+      <ProductReviews product={pageProduct} />
       {/* <SingleShopProductCarousel1
         product={pageProduct}
         otherProducts={products}
