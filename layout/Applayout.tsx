70e486e81e329e7f3730537fe551dbb924a5ff05
BisiDavi <oludavidconnect@gmail.com>
2022-01-23 20:01:00 +0100

    page updated

--- layout/Applayout.tsx
+++ layout/Applayout.tsx
@@ -36,7 +36,7 @@ export default function Applayout({
       `}</Script>
 
       <Head>
-        <title>{title} | Sailfish</title>
+        <title>{title} | Sailfish e-commerce online store </title>
         <script
           src="https://en.trustmate.io/api/widget/4420c1ed-e3a7-47c2-b6a2-2d7386a819da/script"
           defer
