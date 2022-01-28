bf3f0b88953f2b525c12037bac64e182fa5409df
BisiDavi <oludavidconnect@gmail.com>
2022-01-28 10:38:08 +0100

    airwallex updated

--- hooks/useToast.ts
+++ hooks/useToast.ts
@@ -1,4 +1,4 @@
-import { ReactText } from "react";
+import { ReactText, useRef } from "react";
 import { toast } from "react-toastify";
 
 import useLoading from "@/hooks/useLoading";
@@ -37,9 +37,45 @@ export default function useToast() {
     });
   }
 
+  const toastId: any = useRef(null);
+
+  const loadToast = () => {
+    updateLoadingState();
+    toastId.current = toast.loading("Processing ...", {
+      position: "top-left",
+    });
+  };
+
+  const successToast = (text: string) => {
+    updateLoadingState();
+    toast.update(toastId.current, {
+      render: text,
+      type: "success",
+      isLoading: false,
+      position: "top-left",
+      closeButton: true,
+      autoClose: 5000,
+    });
+  };
+
+  const errorToast = (text: string) => {
+    updateLoadingState();
+    toast.update(toastId.current, {
+      render: text,
+      type: "error",
+      isLoading: false,
+      position: "top-left",
+      closeButton: true,
+      autoClose: 5000,
+    });
+  };
+
   return {
     isLoading,
     isSuccessful,
     hasError,
+    loadToast,
+    successToast,
+    errorToast,
   };
 }
