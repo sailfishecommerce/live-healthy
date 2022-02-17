4a179683aa992ef06d2f86e7c30a3cc6de06b76c
BisiDavi <oludavidconnect@gmail.com>
2022-02-17 23:39:20 +0100

    algolia search query updated

--- pages/shop/index.jsx
+++ pages/shop/index.jsx
@@ -1,3 +1,4 @@
+import isEqual from "react-fast-compare";
 import { Component } from "react";
 import { withRouter } from "next/router";
 import qs from "qs";
@@ -7,115 +8,116 @@ import searchClient from "@/lib/algoliaConfig";
 import Applayout from "@/layout/Applayout";
 import MarketplaceTemp from "@/components/MarketplaceTemp";
 import styles from "@/styles/shop.module.css";
+
 const updateAfter = 700;
 
-// const createURL = (state) => `?${qs.stringify(state)}`;
+const createURL = (state) => `?${qs.stringify(state)}`;
 
 const pathToSearchState = (path) =>
   path.includes("?") ? qs.parse(path.substring(path.indexOf("?") + 1)) : {};
 
-// const searchStateToURL = (searchState) =>
-//   searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : "";
+const searchStateToURL = (searchState) =>
+  searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : "";
 
 const DEFAULT_PROPS = {
   searchClient,
   indexName: "New_Livehealthy_products_index",
 };
 
-function getCategorySlug(name) {
-  return name.split(" ").map(encodeURIComponent).join("-");
-}
+// function getCategorySlug(name) {
+//   return name.split(" ").map(encodeURIComponent).join("-");
+// }
 
-function getCategoryName(slug) {
-  return slug.split("-").map(decodeURIComponent).join(" ");
-}
+// function getCategoryName(slug) {
+//   return slug.split("-").map(decodeURIComponent).join(" ");
+// }
 
-const createURL = (state) => {
-  const isDefaultRoute =
-    !state.query &&
-    state.page === 1 &&
-    state.refinementList &&
-    state.refinementList?.vendor.length === 0 &&
-    state.refinementList?.tags.length === 0 &&
-    state?.menu &&
-    !state.menu?.product_type;
+// const createURL = (state) => {
+//   const isDefaultRoute =
+//     !state.query &&
+//     state.page === 1 &&
+//     state.refinementList &&
+//     state.refinementList?.vendor.length === 0 &&
+//     state.refinementList?.tags.length === 0 &&
+//     state?.menu &&
+//     !state.menu?.product_type;
 
-  const categoryPath = state.menu?.product_type
-    ? `${getCategorySlug(state.menu.product_type)}/`
-    : "";
+//   const categoryPath = state.menu?.product_type
+//     ? `${getCategorySlug(state.menu.product_type)}/`
+//     : "";
 
-  const queryParameters = {};
+//   const queryParameters = {};
 
-  if (state.query) {
-    queryParameters.query = encodeURIComponent(state.query);
-  }
+//   if (state.query) {
+//     queryParameters.query = encodeURIComponent(state.query);
+//   }
 
-  if (state.page !== 1) {
-    queryParameters.page = state.page;
-  }
+//   if (state.page !== 1) {
+//     queryParameters.page = state.page;
+//   }
 
-  if (state.refinementList.vendor) {
-    queryParameters.vendor = [state.refinementList.vendor].map(
-      encodeURIComponent
-    );
-  }
+//   if (state.refinementList.vendor) {
+//     queryParameters.vendor = [state.refinementList.vendor].map(
+//       encodeURIComponent
+//     );
+//   }
 
-  if (state.refinementList.tags) {
-    queryParameters.tags = [state.refinementList.tags].map(encodeURIComponent);
-  }
+//   if (state.refinementList.tags) {
+//     queryParameters.tags = [state.refinementList.tags].map(encodeURIComponent);
+//   }
 
-  const queryString = qs.stringify(queryParameters, {
-    addQueryPrefix: true,
-    arrayFormat: "repeat",
-  });
+//   const queryString = qs.stringify(queryParameters, {
+//     addQueryPrefix: true,
+//     arrayFormat: "repeat",
+//   });
 
-  if (isDefaultRoute) {
-    return "";
-  }
+//   if (isDefaultRoute) {
+//     return "";
+//   }
 
-  return `shop${categoryPath}${queryString}`;
-};
+//   return `shop${categoryPath}${queryString}`;
+// };
 
-const searchStateToURL = (searchState) => {
-  console.log("searchState", searchState);
-  const searchValue = searchState ? createURL(searchState) : "";
-  return searchValue;
-};
+// const searchStateToURL = (searchState) => {
+//   console.log("searchState", searchState);
+//   const searchValue = searchState ? createURL(searchState) : "";
+//   return searchValue;
+// };
 
-const urlToSearchState = (location) => {
-  console.log("location", location);
-  const pathnameMatches = location.pathname?.match(/shop\/(.*?)\/?$/);
-  const category = getCategoryName(
-    (pathnameMatches && pathnameMatches[1]) || ""
-  );
-  const {
-    query = "",
-    page = 1,
-    vendors = [],
-    tags = [],
-  } = qs.parse(location?.search?.slice(1));
-  // `qs` does not return an array when there's a single value.
-  const allVendors = Array.isArray(vendors)
-    ? vendors
-    : [vendors].filter(Boolean);
-  const allTags = Array.isArray(tags) ? tags : [tags].filter(Boolean);
-  console.log("allVendors", allVendors, "allTags", allTags);
-  return {
-    query: decodeURIComponent(query),
-    page,
-    menu: {
-      product_type: decodeURIComponent(category),
-    },
-    refinementList: {
-      vendor: allVendors.map(decodeURIComponent),
-      tags: allTags.map(decodeURIComponent),
-    },
-  };
-};
+// const urlToSearchState = (location) => {
+//   console.log("location", location);
+//   const pathnameMatches = location.pathname?.match(/shop\/(.*?)\/?$/);
+//   const category = getCategoryName(
+//     (pathnameMatches && pathnameMatches[1]) || ""
+//   );
+//   const {
+//     query = "",
+//     page = 1,
+//     vendors = [],
+//     tags = [],
+//   } = qs.parse(location?.search?.slice(1));
+//   // `qs` does not return an array when there's a single value.
+//   const allVendors = Array.isArray(vendors)
+//     ? vendors
+//     : [vendors].filter(Boolean);
+//   const allTags = Array.isArray(tags) ? tags : [tags].filter(Boolean);
+//   console.log("allVendors", allVendors, "allTags", allTags);
+//   return {
+//     query: decodeURIComponent(query),
+//     page,
+//     menu: {
+//       product_type: decodeURIComponent(category),
+//     },
+//     refinementList: {
+//       vendor: allVendors.map(decodeURIComponent),
+//       tags: allTags.map(decodeURIComponent),
+//     },
+//   };
+// };
 
 class Shop extends Component {
   state = {
-    searchState: urlToSearchState(this.props.searchState),
+    searchState: this.props.searchState,
     lastRouter: this.props.router,
   };
 
@@ -133,7 +135,7 @@ class Shop extends Component {
     };
   }
   static getDerivedStateFromProps(props, state) {
-    if (state.lastRouter !== props.router) {
+    if (!isEqual(state.lastRouter, props.router)) {
       return {
         searchState: pathToSearchState(props.router.asPath),
         lastRouter: props.router,
@@ -165,13 +167,7 @@ class Shop extends Component {
       });
     }, updateAfter);
 
-    this.setState((prevState) => {
-      console.log("prevState", prevState);
-      return {
-        ...this.state,
-        searchState,
-      };
-    });
+    this.setState({ searchState });
   };
   render() {
     console.log("this.state", this.state);
