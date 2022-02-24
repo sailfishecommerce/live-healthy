8c849f4a97e0300935078575457d07ca9c6705a8
BisiDavi <oludavidconnect@gmail.com>
2022-02-24 01:22:17 +0100

    algolia page recommendation updated

--- components/ProductOverview.tsx
+++ components/ProductOverview.tsx
@@ -1,13 +1,33 @@
+import { useEffect } from "react";
 import ProductBanner from "./ProductBanner";
 import ProductDescription from "./ProductDescription";
 import ProductGalleryDetails from "./ProductGalleryDetails";
 import ProductReviews from "./ProductReviews";
+import { algoliaClientRecommend } from "@/lib/algoliaConfig";
 
 interface ProductOverviewProps {
   pageProduct?: any;
 }
 
 export default function ProductOverview({ pageProduct }: ProductOverviewProps) {
+  console.log("pageProducts", pageProduct);
+
+  useEffect(() => {
+    algoliaClientRecommend
+      .getRelatedProducts([
+        {
+          indexName: "New_Livehealthy_products_index",
+          objectID: pageProduct.ObjectID,
+        },
+      ])
+      .then(({ results }) => {
+        console.log(results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <ProductBanner product={pageProduct} />
--- lib/algoliaConfig.ts
+++ lib/algoliaConfig.ts
@@ -1,6 +1,13 @@
 import algoliasearch from "algoliasearch";
+import algoliarecommend from "@algolia/recommend";
 
 const searchClient = algoliasearch(
+  `${process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID}`,
+  `${process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY}`
+);
+
+
+export const algoliaClientRecommend = algoliarecommend(
   `${process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID}`,
   `${process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY}`
 );
