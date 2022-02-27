c6ae6f2b5ffad7e501dadeb1d5a32d750ce789a7
BisiDavi <oludavidconnect@gmail.com>
2022-02-27 12:36:02 +0100

    404 page updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
 import Applayout from "@/layout/Applayout";
+import Error404 from "@/components/404Error";
 
-export default function Error404() {
+export default function Error404Page() {
   return (
     <Applayout title="Error">
       <Error404 />
