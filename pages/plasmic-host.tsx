d869c98a01d31a6e2a095563ae748cbd1b625f2f
BisiDavi <oludavidconnect@gmail.com>
2022-04-21 08:03:05 +0100

    plasmic host added

---
+++ pages/plasmic-host.tsx
@@ -0,0 +1,17 @@
+import Script from "next/script";
+import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
+import { PLASMIC } from "../plasmic-init";
+
+export default function PlasmicHost() {
+  return (
+    PLASMIC && (
+      <div>
+        <Script
+          src="https://static1.plasmic.app/preamble.js"
+          strategy="beforeInteractive"
+        />
+        <PlasmicCanvasHost />
+      </div>
+    )
+  );
+}
