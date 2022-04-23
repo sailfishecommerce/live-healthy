725f374da618698b113d8e2831a77c25f7a75574
BisiDavi <oludavidconnect@gmail.com>
2022-04-23 06:15:53 +0100

    plasmic page updated

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import Applayout from "@/layout/Applayout";
+
+export default function Error404Page() {
+  return (
+    <Applayout title="Error page">
+      <h1>Error page </h1>
+    </Applayout>
+  );
+}
